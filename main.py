c0dd263b1ecd8a45e90933f8347d2e40ebf7a92f
engrbugs <engrbugs@gmail.com>
2023-08-18 23:37:59 -0700

    2023-08-18-2337 done. v.0.99

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from ini_io import save_history_to_config, retrieve_history_from_config, read_se
 from randomizer import generate_new_random
 
 # Read sensitive information from secret.ini
-# Read sensitive information from secret.ini
 my_user_id, my_twitter_data_path, history_file = read_secret_config()
 
 # List to store all the cleaned full_text values
@@ -39,12 +38,25 @@ def remove_urls(text):
     return cleaned_text
 
 
-# Iterate through each tweet in the DataFrame
-for _, tweet in tweets_df.iterrows():
-    tweet_data = tweet['tweet']
+def process_tweets(tweets_df, history_numbers):
+    for _, tweet in tweets_df.iterrows():
+        tweet_data = tweet['tweet']
+        if tweet_meets_criteria(tweet_data):
+            cleaned_full_text = remove_urls(tweet_data['full_text'])
+            total_tweets.append(cleaned_full_text)
 
-    # Check if the tweet meets the criteria
-    if (
+    cool_progress_bar(21)
+
+    total_tweet_count = len(total_tweets)
+    new_random, random_tweet = get_random_tweet(history_numbers, total_tweet_count)
+
+    cool_print_cyan(f"Total Tweets: {total_tweet_count}")
+    cool_print_cyan(f"Random Tweet Index: {new_random + 1}")
+    cool_print_green('Random Tweet: ' + random_tweet)
+
+
+def tweet_meets_criteria(tweet_data):
+    return (
             "full_text" in tweet_data and
             "entities" in tweet_data and
             "user_mentions" in tweet_data["entities"] and
@@ -52,34 +64,21 @@ for _, tweet in tweets_df.iterrows():
             tweet_data.get("retweeted", False) == False and
             not tweet_data["full_text"].startswith("RT ") and
             not tweet_data["full_text"].startswith("@")
-    ):
-        # Remove URLs from the "full_text"
-        cleaned_full_text = remove_urls(tweet_data['full_text'])
-
-        # Store the cleaned full_text in the list
-        total_tweets.append(cleaned_full_text)
-
-# Print a cool progress bar
-cool_progress_bar(21)
-
-# Print the total number of tweets and the random tweet index
-total_tweet_count = len(total_tweets)
-
-# Randomly select a tweet from total_tweets
-new_random = None
-random_tweet = None
-while random_tweet is None:
-    new_random = generate_new_random(history_numbers, total_tweet_count)
-    history_numbers.append(new_random)
-    if len(history_numbers) > 10:
-        history_numbers.pop(0)  # Keep only the last 10 numbers in history
-    save_history_to_config(history_numbers, history_ini_path)
-    random_tweet = total_tweets[new_random]
+    )
 
 
-cool_print_cyan(f"Total Tweets: {total_tweet_count}")
-cool_print_cyan(f"Random Tweet Index: {new_random + 1}")  # Add +1 to start from 1
+def get_random_tweet(history_numbers_sub, total_tweet_count):
+    new_random = None
+    random_tweet = None
+    while random_tweet is None:
+        new_random = generate_new_random(history_numbers_sub, total_tweet_count)
+        history_numbers.append(new_random)
+        if len(history_numbers_sub) > 10:
+            history_numbers_sub.pop(0)
+        save_history_to_config(history_numbers_sub, history_ini_path)
+        random_tweet = total_tweets[new_random]
+    return new_random, random_tweet
 
-# Print the randomly selected tweet
-random_tweet = total_tweets[new_random]
-cool_print_green('Random Tweet: ' + random_tweet)
+
+if __name__ == '__main__':
+    process_tweets(tweets_df, history_numbers)
